120441e21a7f500c52996f991d378b6a3b5d5730
“dolly500” <dolapoakamo01@gmail.com>
2025-05-31 13:26:56 +0100

    update

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ connectDB();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+}));
+
 app.use(morgan('dev'));
 
 // Initialize passport
